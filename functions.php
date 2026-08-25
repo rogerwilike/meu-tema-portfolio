4eddfecb3cbe6ced81d560fdc7b82d176d80f8b0
Roger <30778244+rogerwilike@users.noreply.github.com>
2026-08-25 18:48:58 -0300

    turning online

--- functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-function carregar_estilos_tema() {
-    // Carrega o style.css principal da raiz do tema
-    wp_enqueue_style('meu-estilo-principal', get_stylesheet_uri());
-}
-add_action('wp_enqueue_scripts', 'carregar_estilos_tema');
